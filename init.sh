3ddccd39222f4f6325882e8c18d4ae159e9a36af
John Papa <johnpapa@users.noreply.github.com>
2016-12-03 10:44:28 -0500

    Lint (#24)
    * lint
    
    * ng cli

--- init.sh
+++ init.sh
@@ -1,45 +1,54 @@
-# ng new event-view--prefix ev -sn
+# ng new event-view -p ev -sn
 #####################
+cd event-view
+echo *** turn off spec generation ***
+ng set defaults.spec.class false
+ng set defaults.spec.component false
+ng set defaults.spec.directive false
+ng set defaults.spec.module false
+ng set defaults.spec.pipe false
+ng set defaults.spec.service false
 echo *** work on the shared folder ***
-ng g cl shared/config
-rm src/app/shared/config.spec.ts
-ng g s shared/entity
-ng g s shared/exception
-ng g p shared/init-caps
-ng g s shared/message
-ng g c shared/filter-text
-ng g s shared/filter-text/filter-text
-ng g c shared/modal
-ng g s shared/modal/modal
-ng g c shared/nav
-ng g cl shared/rxjs-operators -ns
-rm src/app/shared/rxjs-operators.spec.ts
-ng g c shared/spinner
-ng g s shared/spinner/spinner
-mkdir -p src/app/shared/speaker-data
-ng g s shared/speaker-data/speaker
-ng g cl shared/speaker-data/speaker model
-ng g c shared/toast
-ng g s shared/toast/toast
+ng g m core
+rm src/app/core/core.component.*
+ng g s core/auth-guard
+ng g cl core/can-deactivate-guard
+ng g cl core/config
+rm src/app/core/config.spec.ts
+ng g s core/entity
+ng g s core/exception
+ng g s core/message
+ng g s core/module-import-guard
+ng g cl core/preload-strategy
+ng g cl core/rxjs-extensions
+ng g s core/user-profile
+ng g m core/modal
+ng g s core/modal
+ng g m core/nav
+ng g m core/spinner
+ng g s core/spinner
+ng g m core/toast
+ng g s core/toast
+ng g cl core/rxjs-operators
 #####################
-echo *** create dashboard ***
-ng g r dashboard
-ng g c +dashboard/shared/dashboard-button
-#####################
-echo *** create sessions ***
-ng g r sessions
-ng g r +sessions/session-list
-ng g r +sessions/session
-ng g c +sessions/shared/session-button
-ng g s +sessions/shared/session
-ng g cl +sessions/shared/session model
-#####################
-echo *** create speakers ***
-ng g r speakers
-ng g r +speakers/speaker-list
-ng g r +speakers/speaker
-ng g c +speakers/shared/speaker-button
-ng g p +speakers/shared/sort-speakers
-#####################
-mkdir -p src/api
-ng g s ./src/api/in-memory-store
+# echo *** create dashboard ***
+# ng g r dashboard
+# ng g c +dashboard/shared/dashboard-button
+# #####################
+# echo *** create sessions ***
+# ng g r sessions
+# ng g r +sessions/session-list
+# ng g r +sessions/session
+# ng g c +sessions/shared/session-button
+# ng g s +sessions/shared/session
+# ng g cl +sessions/shared/session model
+# #####################
+# echo *** create speakers ***
+# ng g r speakers
+# ng g r +speakers/speaker-list
+# ng g r +speakers/speaker
+# ng g c +speakers/shared/speaker-button
+# ng g p +speakers/shared/sort-speakers
+# #####################
+# mkdir -p src/api
+# ng g s ./src/api/in-memory-store
